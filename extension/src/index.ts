d1cd039d8ca3ce0cd564214c14b2775c2155e7da
the-sociophobic <torikowashi@gmail.com>
2024-10-22 06:56:11 +0300

    add traversePages()

--- extension/src/index.ts
+++ extension/src/index.ts
@@ -1,14 +1,19 @@
 import { insertButton } from './components/button'
-import { postArtistData } from './routes/add-artist'
-import { postPaintingData } from './routes/add-painting'
+// import { postArtistData } from './routes/add-artist'
+// import { postPaintingData } from './routes/add-painting'
+import { traversePages } from './routes/traverse-pages'
 
 
 function main() {
-  if (window.location.href.includes('socrealizm.com.ua/gallery/painting/')) {
-    insertButton(postPaintingData)
-  }
-  if (window.location.href.includes('socrealizm.com.ua/gallery/artist/')) {
-    insertButton(postArtistData)
+  // if (window.location.href.includes('socrealizm.com.ua/gallery/painting/')) {
+  //   insertButton(postPaintingData)
+  // }
+  // if (window.location.href.includes('socrealizm.com.ua/gallery/artist/')) {
+  //   insertButton(postArtistData)
+  // }
+  if (window.location.href.includes('socrealizm.com.ua/gallery')) {
+    // insertButton(traversePages)
+    setTimeout(traversePages, 450)
   }
 }
 
