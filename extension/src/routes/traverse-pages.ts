32fda232e9f6edebcd9851862970708f7def9d1b
the-sociophobic <torikowashi@gmail.com>
2025-05-01 08:28:33 +0300

    add artists title scraping from catalogPage

--- extension/src/routes/traverse-pages.ts
+++ extension/src/routes/traverse-pages.ts
@@ -9,14 +9,18 @@ export const traversePages = async () => {
     const artistsData = [...artistsElements].map((li: Element) => {
       const anchor = li.querySelector('a') as HTMLAnchorElement
       const image = li.querySelector('img') as HTMLImageElement
-      const title = li.querySelector('p')?.textContent || ''
+      const title = [...(li.children?.[0]?.children || [])]
+        .find(child => child.className === 'title')
+        ?.textContent || ''
+      const birthDate = title.match(/\d{4}/g)?.[0]
+      const titleWithoutBirthDate = birthDate ? title.replace(birthDate, '') : title
 
       return {
         url: anchor?.href.replace('https://socrealizm.com.ua/gallery/artist/', ''),
-        title: title,
+        title: titleWithoutBirthDate,
         image: image?.src,
         description: [], // Можно парсить прямо из каталога, если добавишь описание
-        birthDate: title.match(/\d{4}/g)?.[0],
+        birthDate,
         deathDate: null // В каталоге нет этой инфы
       }
     })
